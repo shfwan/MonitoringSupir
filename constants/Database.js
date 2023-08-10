fad8595986b30908c2336b5e5563c308985ff132
shfwan <shafwan367@gmail.com>
2023-08-10 15:21:29 +0900

    Dumy data

---
+++ constants/Database.js
@@ -0,0 +1,21 @@
+const supirData = [
+    {
+        id: 1,
+        nama: "Shafwan M Nurlette",
+        alamat: "Galunggung",
+        phoneNumber: "081218987160"
+    },
+    {
+        id: 2,
+        nama: "Mingki",
+        alamat: "Poka",
+        phoneNumber: "08231487160"
+    },
+    {
+        id: 3,
+        nama: "Ongko",
+        alamat: "Wayame",
+        phoneNumber: "08423459870"
+    },
+]
+
