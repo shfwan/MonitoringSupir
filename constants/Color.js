5d65397c658dae8eb40f160c600828d93f622ba3
shfwan <shafwan367@gmail.com>
2023-08-11 14:46:51 +0900

    Icon js

--- constants/Color.js
+++ constants/Color.js
@@ -1,25 +1,4 @@
-const ICON = {
-    Account: require('../assets/SVG/iconAccount.svg'),
-    ArrowLeft :require('../assets/SVG/iconArrowLeft.svg'),
-    ArrowRight :require('../assets/SVG/iconArrowRight.svg'),
-    Data :require('../assets/SVG/iconData.svg'),
-    Delete :require('../assets/SVG/iconDelete.svg'),
-    Edit :require('../assets/SVG/iconEdit.svg'),
-    Hadir :require('../assets/SVG/iconHadir.svg'), 
-    Home:require('../assets/SVG/iconHome.svg'),
-    Izin :require('../assets/SVG/iconIzin.svg'),
-    Lock :require('../assets/SVG/iconLock.svg'),
-    Notif :require('../assets/SVG/iconNotif.svg'),
-    Plus :require('../assets/SVG/iconPlus.svg') ,
-    Sakit:require('../assets/SVG/iconSakit.svg'),
-    Search :require('../assets/SVG/iconSearch.svg'),
-    Statistik:require('../assets/SVG/iconStatistik.svg'),
-    Waktu :require('../assets/SVG/iconWaktu.svg')
-}
-
-import {} from '../assets/SVG'
-
-const COLORS = {
+const Color = {
     Hijau : "#09A3BE",
     AbuAbu: "#464646",
     Putih : "#FFFFFF",
@@ -27,5 +6,5 @@ const COLORS = {
     BG: "#F3F3"
 }
 
-export {COLORS, ICON}
+export default Color
 
---
+++ constants/Icon.js
@@ -0,0 +1,20 @@
+const Icon = {
+    Account: require('../assets/SVG/iconAccount.svg'),
+    ArrowLeft: require('../assets/SVG/iconArrowLeft.svg'),
+    ArrowRight: require('../assets/SVG/iconArrowRight.svg'),
+    Data: require('../assets/SVG/iconData.svg'),
+    Delete: require('../assets/SVG/iconDelete.svg'),
+    Edit: require('../assets/SVG/iconEdit.svg'),
+    Hadir: require('../assets/SVG/iconHadir.svg'), 
+    Hom: require('../assets/SVG/iconHome.svg'),
+    Izin: require('../assets/SVG/iconIzin.svg'),
+    Lock: require('../assets/SVG/iconLock.svg'),
+    Notif: require('../assets/SVG/iconNotif.svg'),
+    Plus: require('../assets/SVG/iconPlus.svg') ,
+    Saki: require('../assets/SVG/iconSakit.svg'),
+    Search: require('../assets/SVG/iconSearch.svg'),
+    Statisti: require('../assets/SVG/iconStatistik.svg'),
+    Waktu: require('../assets/SVG/iconWaktu.svg')
+}
+
+export default Icon
